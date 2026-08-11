f215633e33e38c7f8c8813f0a09edb48ae8a0e0c
Anne Verona Skovsted <avs292r@gmail.com>
2026-08-11 13:48:07 +0200

    First setup

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import asyncio
 import logging
+import os
 import sys
+import sbsip
+from datafordeler import Datafordeler
+import argparse
 
-from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential, WorkItemStatus
+from automation_server_client import (
+    AutomationServer,
+    Workqueue,
+    WorkItemError,
+    Credential,
+    WorkItemStatus,
+)
+
+proces_navn = "SBSYS-brevsender"
+fordeler: Datafordeler
 
 
 async def populate_queue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from populate workqueue!")
+
+    print("hej")
 
 
 async def process_workqueue(workqueue: Workqueue):
@@ -19,7 +34,7 @@ async def process_workqueue(workqueue: Workqueue):
     for item in workqueue:
         with item:
             data = item.data  # Item data deserialized from json as dict
- 
+
             try:
                 # Process the item here
                 pass
@@ -34,6 +49,33 @@ if __name__ == "__main__":
     workqueue = ats.workqueue()
 
     # Initialize external systems for automation here..
+    sbsip_credential = Credential.get_credential("SBSip - produktion")
+
+    parser = argparse.ArgumentParser(description=proces_navn)
+
+    parser.add_argument(
+        "--queue",
+        action="store_true",
+        help="Udfyld køen og afslut",
+    )
+
+    certifikat_sti = os.getenv("CERTIFICATES", "certificates")
+    fordeler = Datafordeler(
+        certifikat_sti=os.path.join(certifikat_sti, "datafordeler.crt"),
+        certifikat_nøglefil=os.path.join(certifikat_sti, "datafordeler.key"),
+    )
+
+    parser.add_argument(
+        "--word-template",
+        default="input\brev_template.docx",
+        help="Path to the Word template for letter generation",
+    )
+    args = parser.parse_args()
+
+    sbsip.start_sbsip(
+        brugernavn=sbsip_credential.username,
+        adgangskode=sbsip_credential.password,
+    )
 
     # Queue management
     if "--queue" in sys.argv:
